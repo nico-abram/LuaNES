126bb2e213ddf766aa2e62cf6251893d453ad0ab
Nicolas <abramlujan@gmail.com>
2019-01-17 16:51:55 -0300

    Formatting

--- main.lua
+++ main.lua
@@ -18,15 +18,17 @@ function love.load(arg)
     image = love.graphics.newImage(imageData)
     love.window.setTitle("LuaNEs")
     --Nes = NES:new({file="tests/hello.nes", loglevel=5})
-    Nes = NES:new({
-        file = file, 
-        loglevel = loglvl, 
-        pc = pc, 
-        palette = UTILS.map(
-            PALETTE:defacto_palette(), 
-            function(c) 
-                return {c[1]/256,c[2]/256,c[3]/256}
-            end
+    Nes =
+        NES:new(
+        {
+            file = file,
+            loglevel = loglvl,
+            pc = pc,
+            palette = UTILS.map(
+                PALETTE:defacto_palette(),
+                function(c)
+                    return {c[1] / 256, c[2] / 256, c[3] / 256}
+                end
             )
         }
     )
@@ -44,32 +46,56 @@ local keyButtons = {
     ["i"] = Pad.SELECT,
     ["return"] = Pad.START
 }
-function love.keypressed( key )
-    for k,v in pairs(keyButtons) do
+function love.keypressed(key)
+    for k, v in pairs(keyButtons) do
         if k == key then
-      keyEvents[#keyEvents+1] = {"keydown", v}
+            keyEvents[#keyEvents + 1] = {"keydown", v}
         end
     end
 end
- 
-function love.keyreleased( key )
-    for k,v in pairs(keyButtons) do
+
+function love.keyreleased(key)
+    for k, v in pairs(keyButtons) do
         if k == key then
-      keyEvents[#keyEvents+1] = {"keyup", v}
+            keyEvents[#keyEvents + 1] = {"keyup", v}
         end
     end
 end
 
 love.frame = 0
-function love.draw()
-    for i,v in ipairs(keyEvents) do
-        Nes.pads[v[1]](Nes.pads,1, v[2])
+local time = 0
+local timeTwo = 0
+local rate = 1 / 60
+local fps = 0
+local fpstmp = 0
+local pixelCount = PPU.SCREEN_HEIGHT * PPU.SCREEN_WIDTH
+local function update()
+    drawn = true
+    fpstmp = fpstmp + 1
+    for i, v in ipairs(keyEvents) do
+        Nes.pads[v[1]](Nes.pads, 1, v[2])
     end
     keyEvents = {}
     Nes:run_once()
+end
+function love.draw()
+    --[[
+    time = time + love.timer.getDelta()
+    timeTwo = timeTwo + love.timer.getDelta()
+    if time > rate then
+        time = 0
+        update()
+    end
+    if timeTwo > 1 then
+        timeTwo = 0
+        fps = fpstmp
+        fpstmp = 0
+    end
+    --]]
+    update()
     --[
     local pxs = Nes.cpu.ppu.output_pixels
-    for i = 1, #pxs do
+    for i = 1, pixelCount do
         local x = (i - 1) % width
         local y = math.floor((i - 1) / width) % height
         local px = pxs[i]
@@ -82,11 +108,6 @@ function love.draw()
         local r = px[1]
         local g = px[2]
         local b = px[3]
-        --]]
-        local xx = 1 + pixSize * (x)
-        local yy = 1 + pixSize * (y)
-        imageData:setPixel(xx, yy, px[1], px[2], px[3], 1)
-        --[[
         for j = 0, pixSize - 1 do
             for k = 0, pixSize - 1 do
                 local xx = 1 + pixSize * (x) + j
@@ -94,6 +115,11 @@ function love.draw()
                 imageData:setPixel(xx, yy, r, g, b, 1)
             end
         end
+        --]]
+        --[
+        local xx = 1 + (x)
+        local yy = 1 + (y)
+        imageData:setPixel(xx, yy, px[1], px[2], px[3], 1)
         --]]
     end
     -- draw palette
@@ -127,5 +153,5 @@ function love.draw()
         love.profiler.reset()
     end
     --]]
-    love.graphics.print("Current FPS: " .. tostring(love.timer.getFPS()), 10, 10)
+    love.graphics.print("Current FPS: " .. tostring(love.timer.getFPS()) .. " " .. tostring(fps), 10, 10)
 end
